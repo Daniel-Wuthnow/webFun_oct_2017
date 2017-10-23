d66dd50108a042e1256c34df549ea64b481f336e
Daniel <003wuthnowd@gmail.com>
2017-10-23 13:51:15 -0500

    first JS try

---
+++ Daniel_Wuthnow/i_do_declare/i_do_declare.js
@@ -0,0 +1,17 @@
+var b_month = 12;
+var b_day = 27;
+var b_year = 94;
+var food = "potato";
+var movie = "Lord of the Rings";
+var book = "zero";
+var ilovemywife = true;
+var iliketoread = false;
+
+console.log(`I was born on the ${b_day} day`)
+console.log(`of the ${b_month} month`)
+console.log(`of the year ${b_year}`)
+console.log(`My favrite food is a ${food}`)
+console.log(`My favrite movie is ${movie}`)
+console.log(`I have read ${book} books`)
+console.log(`I love my wife ${ilovemywife}`)
+console.log(`I have a passion for reading ${iliketoread}`)
