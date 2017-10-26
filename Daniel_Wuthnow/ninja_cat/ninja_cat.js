8465cb7febecabf6d555d4d1e27001e28e28f853
Daniel <003wuthnowd@gmail.com>
2017-10-26 13:37:38 -0500

    adding ninja_cat

---
+++ Daniel_Wuthnow/ninja_cat/ninja_cat.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('img').click(function() {
+
+	//gets the attrubute for cat images
+  		var newpic = $(this).attr('data-alt-src');
+  	//makes temp the ninja pics
+  		var temp = $(this).attr('src');
+  		$(this).attr('src', newpic)
+  		$(this).attr('data-alt-src', temp)
+	});
+});
